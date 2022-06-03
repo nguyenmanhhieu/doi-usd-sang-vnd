36282e6f618b7b21fe860c8e9f2a4737ad2f306e
nguyenmanhhieu <hieunm14092001@gmail.com>
2022-06-03 22:39:53 +0700

    chuyen doi usd sang vnd
    nguyen manh hieu

---
+++ chuyen doi usd sang vnd.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Chuyen doi tỷ gia dola sang vnd
+//
+//  Created by Hiếu Hee on 03/06/2022.
+//
+
+#include <stdio.h>
+int main(void)
+{
+    int usd;
+    int vnd;
+        printf("nhap usd\n");
+        scanf("%d",&usd);
+    vnd=usd*23000;
+        printf("vnd is %d\n",vnd);
+   return 0;
+}
